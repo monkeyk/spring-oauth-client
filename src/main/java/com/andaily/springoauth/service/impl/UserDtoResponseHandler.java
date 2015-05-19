382bae630612cf1967cbaa5c5791315e467073dc
lishengzhao <shengzhao@honyee.cc>
2015-05-19 14:45:42 +0800

    00010	client implement 'authorization_code': resource

--- src/main/java/com/andaily/springoauth/service/impl/UserDtoResponseHandler.java
+++ src/main/java/com/andaily/springoauth/service/impl/UserDtoResponseHandler.java
@@ -5,6 +5,12 @@ import com.andaily.springoauth.infrastructure.httpclient.MkkHttpResponse;
 import com.andaily.springoauth.infrastructure.json.JsonUtils;
 import com.andaily.springoauth.service.dto.UserDto;
 import org.apache.http.StatusLine;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 /**
  * 15-5-18
@@ -13,6 +19,7 @@ import org.apache.http.StatusLine;
  */
 public class UserDtoResponseHandler implements HttpResponseHandler {
 
+    private static final String ERROR_DATA_KEY = "<oauth>";
 
     private UserDto userDto;
 
@@ -29,15 +36,63 @@ public class UserDtoResponseHandler implements HttpResponseHandler {
     @Override
     public void handleResponse(MkkHttpResponse response) {
         if (response.isResponse200()) {
-            final String text = response.responseAsString();
-            this.userDto = JsonUtils.textToBean(new UserDto(), text);
+            responseToUserDto(response);
         } else {
             final StatusLine statusLine = response.httpResponse().getStatusLine();
             this.userDto = new UserDto(String.valueOf(statusLine.getStatusCode()), statusLine.getReasonPhrase());
         }
     }
 
+    private void responseToUserDto(MkkHttpResponse response) {
+        final String text = response.responseAsString();
+        if (text.startsWith(ERROR_DATA_KEY)) {
+            parseErrorXML(response);
+        } else {
+            this.userDto = JsonUtils.textToBean(new UserDto(), text);
+        }
+    }
+
+    private void parseErrorXML(MkkHttpResponse response) {
+        try {
+            final SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
+            saxParser.parse(response.httpResponse().getEntity().getContent(), new ErrorDefaultHandler());
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
     public UserDto getUserDto() {
         return userDto;
     }
+
+
+    /**
+     * Parse Error XML handler
+     */
+    private class ErrorDefaultHandler extends DefaultHandler {
+        private String qName;
+
+        private ErrorDefaultHandler() {
+            userDto = new UserDto();
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            this.qName = qName;
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            final String text = new String(ch, start, length);
+
+            if ("error_description".equalsIgnoreCase(qName)) {
+                userDto.setErrorDescription(text);
+            } else if ("error".equalsIgnoreCase(qName)) {
+                userDto.setError(text);
+            }
+
+        }
+
+    }
+
 }
---
+++ src/main/webapp/WEB-INF/jsp/resources/unity_user_info.jsp
@@ -0,0 +1,34 @@
+<%--
+ * 
+ * @author Shengzhao Li
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>User Info [Unity]</title>
+</head>
+<body>
+<a href="${contextPath}/">Home</a>
+
+<h2>User Info [Unity]
+    <small>The data from Oauth Server API</small>
+</h2>
+
+<dl class="dl-horizontal">
+    <dt>username</dt>
+    <dd><code>${userDto.username}</code></dd>
+    <dt>guid</dt>
+    <dd><code>${userDto.guid}</code></dd>
+    <dt>phone</dt>
+    <dd><code>${userDto.phone}</code></dd>
+    <dt>email</dt>
+    <dd><code>${userDto.email}</code></dd>
+    <dt>privileges</dt>
+    <dd><code>${userDto.privileges}</code></dd>
+
+</dl>
+
+<a href="javascript:history.back();" class="btn btn-default">Back</a>
+</body>
+</html>
