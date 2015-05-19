0dece3305daa36e8c396dd6c6269b1db33e80519
lishengzhao <shengzhao@honyee.cc>
2015-05-19 13:23:17 +0800

    Refactor authorization_code  controller

--- src/main/java/com/andaily/springoauth/web/UnityController.java
+++ src/main/java/com/andaily/springoauth/web/UnityController.java
@@ -8,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
+ * Local resource actions.
+ * No relation with Oauth
+ *
  * @author Shengzhao Li
  */
 @Controller
