c36f3f0b89ebef6b63a5b213d255d19dbdf258e9
shengzhaoli.shengz <monkeyk1987@gmail.com>
2023-11-06 23:17:08 +0800

    refactor codes;  springboot

---
+++ src/main/java/com/andaily/springoauth/SpringOAuthClientApplication.java
@@ -0,0 +1,24 @@
+package com.andaily.springoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 2023/11/6 22:17
+ *
+ * @author Shengzhao Li
+ * @since 2.0.0
+ */
+@SpringBootApplication
+public class SpringOAuthClientApplication {
+
+    /**
+     * 主函数入口，用于启动程序
+     *
+     * @param args 命令行参数
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOAuthClientApplication.class, args);
+    }
+
+}
